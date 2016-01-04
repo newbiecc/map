014836e853344a6d33ece63e4385f32eefdb0d00
Ching Kim Fu Cliff <Cliff@chings-macbook-pro-3.local>
2016-01-04 21:42:54 +0800

    Initial Commit

---
+++ DevBootcamps/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DevBootcamps
+//
+//  Created by Ching Kim Fu Cliff on 1/4/16.
+//  Copyright © 2016 Ching Kim Fu Cliff. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DevBootcamps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DevBootcamps
+//
+//  Created by Ching Kim Fu Cliff on 1/4/16.
+//  Copyright © 2016 Ching Kim Fu Cliff. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
